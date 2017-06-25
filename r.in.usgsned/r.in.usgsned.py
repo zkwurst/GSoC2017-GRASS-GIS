841431b5328416f3e6995ce4c671cc9b65c14ec3
zkwurst <zkwurst@gmail.com>
2017-06-25 18:56:53 -0400

    06-25-17

--- r.in.usgsned/r.in.usgsned.py
+++ r.in.usgsned/r.in.usgsned.py
@@ -47,8 +47,8 @@ NOTES:      Needs:
 #%option
 #% key: product
 #% required: yes
-#% options: NED, other
-#% answer: NED
+#% options: National Elevation Dataset (NED), other
+#% answer: National Elevation Dataset (NED)
 #% label: Select USGS Data Product
 #% description: Choose which available USGS datasets to query
 #% guisection: USGS Data Selection
@@ -74,9 +74,16 @@ NOTES:      Needs:
 #% guisection: USGS Data Selection
 #%end
 
+#%option G_OPT_M_MAPSET
+#% key: mapset
+#% description: USGS Mapset
+#% answer: USGS Mapset
+#% guisection: Download
+#%end
+
 #%option G_OPT_R_OUTPUT
 #% key: output_file
-#% description: New Map Name
+#% description: New Composite Tile Map Name
 #% answer:composite_tile.img
 #% gisprompt: new,bin,file
 #% guisection: Download
@@ -109,7 +116,7 @@ import sys
 import os
 import zipfile
 import grass.script as gscript
-#import urllib
+import urllib
 #from Tkinter import *
 #from tkFileDialog import *
 #import time
@@ -119,6 +126,7 @@ def main():
     gui_product = options['product']
     gui_resolution = options['resolution']
     gui_format = options['format']
+    gui_mapset = options['mapset']
     gui_output = options['output_file']
     gui_region = options['region']
     gui_method = options['method']
@@ -138,55 +146,11 @@ def main():
 #    composite_tile.img
 #    nearest
 #    False
-    
-    
-    
 
     print "\n************************\nNED Data Download Module\n\
     ************************"
     
-    
-    # GUI dialogue to specify working DIR
-    
-    
-    
-#"""    
-#    # Set working directory 
-#    home_dir = os.path.expanduser('~')
-#    root = Tk()
-#    root.withdraw()
-#    root.update()
-#    work_DIR = askdirectory(initialdir=home_dir, title="Please specify local download directory:")
-#    
-#    try:
-#        os.chdir(work_DIR)
-#        print "\nDownload directory:\t" + work_DIR
-#        # root.destroy()
-#    except:
-#        if OSError:
-#            print OSError
-#            sys.exit(1)
-#        if os.path.exists(work_DIR):
-#            os.chdir(work_DIR)
-#            print "\nDownload directory:\t" + work_DIR
-#            root.destroy()
-#        else:
-#            os.makedirs(work_DIR)
-#            print "\nDownload directory:\t" + work_DIR
-#            root.destroy()
-#    
-#    # Ask to select or set desired g.region
-#    # current region is default
-#"""
-
-#"""
-#New attempt at getting map projection and region coordinates translated
-#into NAD83 for TNM API query. Not working currently.
-#
-#My goal is to get the region coordinates from a given location, translate
-#them into NAD83, send the TNM API query, and use r.proj to reproject the
-#tiles when they're downloaded. I can't get m.proj to work correctly.
-#"""
+    # GUI dialogue to specify working DIR?
 
     # Check coordinate projection
     gproj = gscript.parse_command('g.proj', flags='g')
@@ -194,26 +158,7 @@ def main():
     gproj_ellipse = gproj['ellps']
     print "\nComputational Region SRS:\nName:\t\t{0}\nEllipse:\t{1}".format(
                                                gproj_datum, gproj_ellipse)
-
-#   #Does not return useable string. 
-    current_proj4 = gscript.parse_command('g.proj', flags='-jf')
     
-    usgs_ned_proj4 = "+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs"
-
-#   if SRS_datum != 'nad83':
-        
-    usgs_ned_proj4 = "+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs"
-
-#   #Does not return useable string. 
-    #current_proj4 = gscript.parse_command('g.proj', flags='-jf')
-
-# Different test methods of hardcoding variable input
-    ###current_proj4 = str(gscript.parse_command('g.proj', flags='jf'))
-    ###str_current_proj4 = current_proj4.replace(':', '=')
-
-    str_current_proj4 = "+proj=lcc +lat_1=36.16666666666666 +lat_2=34.33333333333334 +lat_0=33.75 +lon_0=-79 +x_0=609601.22 +y_0=0 +ellps=GRS80 +units=m +no_defs"
-    ###str_current_proj4 = "+proj=lcc +lat_1=36.16666666666666 +lat_2=34.33333333333334 +lat_0=33.75 +lon_0=-79 +x_0=609601.22 +y_0=0 +no_defs +a=6378137 +rf=298.257222101 +towgs84=0.000,0.000,0.000 +to_meter=1"
-
     gregion = gscript.parse_command('g.region', flags='bg')
     gregion_dict = {}
     for key, value in gregion.iteritems():
@@ -233,117 +178,110 @@ def main():
     else:
         maxx = gregion_dict['ll_e']
         minx = gregion_dict['ll_w']
-        
     float_bbox = [minx, miny, maxx, maxy]
-    min_bbox = [minx, miny]
-    max_bbox = [maxx, maxy]
-    #list_bbox = ",".join((str(coord) for coord in float_bbox))
-    print "min_bbox = {0}".format(min_bbox)
-    print "max_bbox = {0}".format(max_bbox)
-
-#"""
-#I guess there has to be a better way to do this?
-#Both min and max outputs end up being the same. I tried a bunch of
-#different methods to figure it out and couldn't.
-#"""
-
-    min_nad83_coords = gscript.parse_command('m.proj', coordinates=min_bbox, proj_in=str_current_proj4, proj_out=usgs_ned_proj4, separator=',', flags='d')
-    max_nad83_coords = gscript.parse_command('m.proj', coordinates=max_bbox, proj_in=str_current_proj4, proj_out=usgs_ned_proj4, separator=',', flags='d')
     
-    print "min_nad83_coords = {0}".format(min_nad83_coords)
-    print "max_nad83_coords = {0}".format(max_nad83_coords)
+    if gproj_datum == 'wgs84':
+        str_bbox = ",".join((str(coord) for coord in float_bbox))
+    else:
+        #str_current_proj4 = gscript.read_command('g.proj', flags='jf')
+        #print "Origin PROJ.4 string:\n{0}".format(str_current_proj4)
+        # EPSG:4269
+        usgs_ned_proj4 = "+proj=longlat +ellps=GRS80 +datum=NAD83 +nodefs"
+        wgs84_proj4 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+        #print "USGS PROJ.4 string:\n{0}".format(usgs_ned_proj4)
+        
+        min_bbox = [minx, miny]
+        max_bbox = [maxx, maxy]
+        print "min_bbox = {0}".format(min_bbox)
+        print "max_bbox = {0}".format(max_bbox)
 
+        min_nad83_coords = gscript.read_command('m.proj', coordinates=min_bbox,\
+                                                proj_in=wgs84_proj4, \
+                                                proj_out=usgs_ned_proj4, \
+                                                separator='comma', flags='d')
+        max_nad83_coords = gscript.read_command('m.proj', coordinates=max_bbox,\
+                                                proj_in=wgs84_proj4, \
+                                                proj_out=usgs_ned_proj4, \
+                                                separator='comma', flags='d')
+        
+        min_list = min_nad83_coords.split(',')[:2]
+        max_list = max_nad83_coords.split(',')[:2]
+        list_bbox = min_list + max_list
+        str_bbox = ",".join((str(coord) for coord in list_bbox))
+    print str_bbox
 
-
-
-# Old version of converting coordinate output into TNM API format
-#"""      
-#    # If proj not NAD83, temporarily convert to NAD83 to get correct g.region
-#    # convert g.region bounding box coords only, then reproject the tiles?
-#    
-#    # Call "g.region -bg" to get computational region coords
-#    gregion = gscript.parse_command('g.region', flags='bg')
-#    gregion_dict = {}
-#    for key, value in gregion.iteritems():
-#        gregion_keys = ['ll_n', 'll_s', 'll_w', 'll_e']
-#        if key in gregion_keys:
-#            gregion_dict[str(key)] = float(value)
-#    
-#    if gregion_dict['ll_n'] > gregion_dict['ll_s']:
-#        maxy = gregion_dict['ll_n']
-#        miny = gregion_dict['ll_s']
-#    else:
-#        maxy = gregion_dict['ll_s']
-#        miny = gregion_dict['ll_n']
-#    if gregion_dict['ll_w'] > gregion_dict['ll_e']:
-#        maxx = gregion_dict['ll_w']
-#        minx = gregion_dict['ll_e']
-#    else:
-#        maxx = gregion_dict['ll_e']
-#        minx = gregion_dict['ll_w']
-#        
-#    float_bbox = [minx, miny, maxx, maxy]
-#    list_bbox = ",".join((str(coord) for coord in float_bbox))
-#    print "\nComputational Region Coordinates\n{0}".format(float_bbox)
-#"""    
+    gui_prod_str = gui_product + " " + gui_resolution
+    datasets = urllib.quote_plus(gui_prod_str)
+    #bbox = urllib.quote_plus(str_bbox)
+    prodFormats = urllib.quote_plus(gui_format)
     
+    print gui_prod_str
+    print datasets
+    #print bbox
+    print prodFormats
     
+    base_TNM = "https://viewer.nationalmap.gov/tnmaccess/api/products?"
+    datasets_TNM = "datasets={0}".format(datasets)
+    bbox_TNM = "&bbox={0}".format(str_bbox)
+    prodFormats_TNM = "&prodFormats={0}".format(prodFormats)
+    TNM_API_URL = base_TNM + datasets_TNM + bbox_TNM + prodFormats_TNM
     
+    print TNM_API_URL
     
-#    
-#    # Need to not hardcode these variables
-#    datasets = urllib.quote_plus('National Elevation Dataset (NED) 1/3 arc-second')
-#    bbox = urllib.quote_plus(list_bbox)
-#    prodFormats = urllib.quote_plus('IMG')
-#    
-#    base_TNM = "https://viewer.nationalmap.gov/tnmaccess/api/products?"
-#    datasets_TNM = "datasets={0}".format(datasets)
-#    bbox_TNM = "&bbox={0}".format(bbox)
-#    prodFormats_TNM = "&prodFormats={0}".format(prodFormats)
-#    TNM_API_URL = base_TNM + datasets_TNM + bbox_TNM + prodFormats_TNM
-#    
-#    # Query TNM API for data availability
-#    try:
-#        TNM_API_GET = requests.get(TNM_API_URL, timeout=12)
-#        returnJSON = TNM_API_GET.json()
-#        # Parse JSON to return download URL
-#        tile_APIcount = int(returnJSON['total'])
-#        dwnld_size = []
-#        dwnld_URL = []
-#        dataset_name = []
-#        if tile_APIcount > 0:
-#            # Ask for user permission to proceed:
-#                # Give estimated file size, time to complete
-#            for tile in returnJSON['items']:
-#                dwnld_size.append(int(tile['sizeInBytes']))
-#                dwnld_URL.append(str(tile['downloadURL']))
-#                if tile['datasets'][0] not in dataset_name:
-#                    if len(dataset_name) <= 1:
-#                        dataset_name.append(str(tile['datasets'][0]))
-#                    else:
-#                        print "Incompatible datasets detected."
-#                        sys.exit(1)
-#        else:
-#            print "NED imagery is not available for the given parameters."
-#            sys.exit(1)
-#        print "\nDataset:\t{0}".format(dataset_name)
-#        print "# of tiles:\t{0}".format(str(tile_APIcount))
-#        total_size = sum(dwnld_size)
-#        if 6 < len(str(total_size)) < 10:
-#            print "Download Size:\t{0} MB".format(total_size * 1e-6)
-#        if len(str(total_size)) >= 10:
-#            print "Download Size:\t{0} GB".format(total_size * 1e-9)
-#        # ask user permission before proceeding
+    # Query TNM API for data availability
+    try:
+        TNM_API_GET = requests.get(TNM_API_URL, timeout=12)
+        returnJSON = TNM_API_GET.json()
+        # Parse JSON to return download URL
+        tile_APIcount = int(returnJSON['total'])
+        dwnld_size = []
+        dwnld_URL = []
+        dataset_name = []
+        if tile_APIcount > 0:
+            # Ask for user permission to proceed:
+                # Give estimated file size, time to complete
+            for tile in returnJSON['items']:
+                dwnld_size.append(int(tile['sizeInBytes']))
+                dwnld_URL.append(str(tile['downloadURL']))
+                if tile['datasets'][0] not in dataset_name:
+                    if len(dataset_name) <= 1:
+                        dataset_name.append(str(tile['datasets'][0]))
+                    else:
+                        print "Incompatible datasets detected."
+                        sys.exit(1)
+        if tile_APIcount == 0:
+            print "Zero tiles returned. Please check input parameters."
+            sys.exit(1)
+        print "\nDataset:\t{0}".format(dataset_name)
+        print "# of tiles:\t{0}".format(str(tile_APIcount))
+        total_size = sum(dwnld_size)
+        if 6 < len(str(total_size)) < 10:
+            print "Download Size:\t{0} MB".format(total_size * 1e-6)
+        if len(str(total_size)) >= 10:
+            print "Download Size:\t{0} GB".format(total_size * 1e-9)
+        # ask user permission before proceeding
 #        proceed = raw_input("\nWould you like to download this data? (y/n):\t")
 #        if proceed == "n":
 #            print "\nDownload cancelled."
 #            sys.exit(1)
 #        if proceed == "y":
 #            print "\nPlease wait.\nDownloading NED Data...\n"
-#    except requests.exceptions.Timeout:
-#        print "\nUSGS API query has timed out.\n"
-#        sys.exit(1)
+    except requests.exceptions.Timeout:
+        print "\nUSGS API query has timed out.\n"
+        sys.exit(1)
+#"""    
+#    current_gisdbase = gscript.read_command('g.gisenv', get='GISDBASE')
+#    current_location = gscript.read_command('g.gisenv', get='LOCATION_NAME')
 #    
+#    gscript.run_command('g.mapset', mapset=gui_mapset, \
+#                        location=current_location, \
+#                        dbase=current_gisdbase, flags='c')
+#    
+#    new_usgs_mapset = gscript.read_command('g.gisenv', get='MAPSET')
+#    
+#    #work_DIR = current_gisdbase + '/' + current_location + '/' + new_usgs_mapset
+#    #print work_DIR
+#"""   
 #    LT_count = 0
 #    LT_fullpaths = []
 #    LT_basenames = []
